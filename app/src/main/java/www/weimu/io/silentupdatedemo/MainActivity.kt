6365fe3af8b90b1c04d4f0162bb275e97fde3d7b
艹羊 <779213209@qq.com>
2018-01-23 01:29:14 +0800

    删除无用引用

--- app/src/main/java/www/weimu/io/silentupdatedemo/MainActivity.kt
+++ app/src/main/java/www/weimu/io/silentupdatedemo/MainActivity.kt
@@ -4,8 +4,6 @@ import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
-import www.weimu.io.silentupdate.SilentUpdate
-import www.weimu.io.silentupdate.core.AppUpdatePreB
 
 
 /**
