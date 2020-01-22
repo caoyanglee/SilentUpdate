2bd53189d3b753ee70460d39b9109b125a04b721
你需要一台永动机 <779213209@qq.com>
2020-01-22 22:17:48 +0800

    🛠创建频道使用同步库的代码

--- lib/src/main/java/com/pmm/silentupdate/SilentUpdate.kt
+++ lib/src/main/java/com/pmm/silentupdate/SilentUpdate.kt
@@ -1,19 +1,12 @@
 package com.pmm.silentupdate
 
 import android.app.Application
-import android.app.NotificationChannel
-import android.app.NotificationManager
-import android.content.Context
-import android.graphics.Color
-import android.os.Build
-import androidx.annotation.RequiresApi
 import com.pmm.silentupdate.core.*
-import com.pmm.silentupdate.core.Const
-import com.pmm.silentupdate.core.SPCenter
 import com.pmm.silentupdate.strategy.MobileUpdateStrategy
 import com.pmm.silentupdate.strategy.UpdateStrategy
 import com.pmm.silentupdate.strategy.WifiUpdateStrategy
 import com.pmm.ui.helper.FileHelper
+import com.pmm.ui.ktx.createNotificationChannel
 import com.pmm.ui.ktx.getAppName
 import com.pmm.ui.ktx.isConnectWifi
 
@@ -36,8 +29,12 @@ object SilentUpdate {
         //上下文初始化
         ContextCenter.init(context)
         //增加通知频道【兼容8.0】
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O)
-            createNotificationChannel()
+        val channelName = context.getString(R.string.module_silentupdate_channelName)
+        context.createNotificationChannel(
+                channelId = Const.NOTIFICATION_CHANNEL_ID,
+                channelName = channelName,
+                channelDesc = channelName
+        )
     }
 
 
@@ -102,32 +99,6 @@ object SilentUpdate {
         val context = ContextCenter.getAppContext()
         val path = "${Const.UPDATE_FILE_DIR}${context.getAppName()}_v$version.apk"
         return FileHelper.deleteFile(path)
-    }
-
-    /**
-     * 增加通知栏的频道
-     */
-    @RequiresApi(Build.VERSION_CODES.O)
-    private fun createNotificationChannel() {
-        val context = ContextCenter.getAppContext()
-        val mNotificationManager = context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
-        // 通知渠道的id
-        // 用户可以看到的通知渠道的名字.
-        val name = "${context.getAppName()}更新专用"
-        // 用户可以看到的通知渠道的描述
-        val description = "${context.getAppName()}更新专用"
-        val importance = NotificationManager.IMPORTANCE_HIGH
-        val mChannel = NotificationChannel(Const.NOTIFICATION_CHANNEL_ID, name, importance)
-        // 配置通知渠道的属性
-        mChannel.description = description
-        // 设置通知出现时的闪灯（如果 android 设备支持的话）
-        mChannel.enableLights(true)
-        mChannel.lightColor = Color.RED
-        // 设置通知出现时的震动（如果 android 设备支持的话）
-        mChannel.enableVibration(true)
-        mChannel.vibrationPattern = longArrayOf(100, 200, 300, 400, 500, 400, 300, 200, 400)
-        //最后在notificationmanager中创建该通知渠道
-        mNotificationManager.createNotificationChannel(mChannel);
     }
 
 }
